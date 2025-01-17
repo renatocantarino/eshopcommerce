02bd826dfd8f7e6cbe1e5c6fede73fdaa487f98f
renatocantarino <renato.cantarino@gmail.com>
2025-01-17 17:25:06 -0300

    add validators

--- src/Service/Catalog/Catalog.API/Products/Create/CreateCommandValidator.cs
+++ src/Service/Catalog/Catalog.API/Products/Create/CreateCommandValidator.cs
@@ -9,6 +9,6 @@ public class CreateCommandValidator : AbstractValidator<CreateCommand>
         RuleFor(x => x.Name).NotEmpty().WithMessage("{PropertyName} is required");
         RuleFor(x => x.ImageUrl).NotEmpty().WithMessage("{PropertyName} is required");
         RuleFor(x => x.Price).GreaterThan(0).WithMessage("{PropertyName} must be greater than 0");
-        RuleFor(x => x.Categories).NotEmpty().WithMessage("{PropertyName} is required");
+        RuleForEach(x => x.Categories).NotEmpty().WithMessage("{PropertyName} is required");
     }
 }
---
+++ src/Service/Catalog/Catalog.API/Products/Update/UpdateCommandValidator.cs
@@ -0,0 +1,15 @@
+﻿using FluentValidation;
+
+namespace Catalog.API.Products.Update
+{
+    public class UpdateCommandValidator : AbstractValidator<UpdateProductCommand>
+    {
+        public UpdateCommandValidator()
+        {
+            RuleFor(x => x.Id).NotNull().NotEmpty().WithMessage("{PropertyName} is required"); ;
+            RuleFor(x => x.ImageUrl).NotEmpty().WithMessage("{PropertyName} is required");
+            RuleFor(x => x.Price).GreaterThan(0).WithMessage("{PropertyName} must be greater than 0");
+            RuleForEach(x => x.Categories).NotEmpty().WithMessage("{PropertyName} is required");
+        }
+    }
+}
